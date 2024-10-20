6727ce7315eed2e0d1a9363343d2df865ac0183c
Gustitreshana <gustith2105@gmail.com>
2024-10-21 03:47:41 +0700

    feat: add Promo section carousel

--- amimum/src/app/page.tsx
+++ amimum/src/app/page.tsx
@@ -1,11 +1,68 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
 import Icon from "@/components/Icon";
 import Search from "@/components/Search";
+import PromoCard from "@/components/PromoCard";
 
-const Homepage = () => {
+const SimpleCarousel = ({
+  items,
+  itemsToShow,
+  interval = 4000,
+}: {
+  items: any[];
+  itemsToShow: number;
+  interval?: number;
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const slideInterval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + itemsToShow) % items.length);
+    }, interval);
+
+    return () => clearInterval(slideInterval);
+  }, [itemsToShow, items.length, interval]);
+  
+  return (
+    <div className="carousel">
+      <div className="carousel-items" style={{ transform: `translateX(-${currentIndex * (100 / itemsToShow)}%)` }}>
+        {items.map((item, index) => (
+          <div key={index} className="carousel-item">
+            {item}
+          </div>
+        ))}
+      </div>
+      <div className="carousel-indicators flex justify-center mt-2">
+        {Array.from({ length: Math.ceil(items.length / itemsToShow) }).map((_, index) => (
+          <button
+            key={index}
+            className={`w-2 h-2 rounded-full mx-1 mt-2 ${index === Math.floor(currentIndex / itemsToShow) ? 'bg-customGreen1' : 'bg-gray-300'}`}
+            onClick={() => setCurrentIndex(index * itemsToShow)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Home = () => {
+  const promoItems = [
+    <PromoCard />,
+    <PromoCard />,
+    <PromoCard />,
+    <PromoCard />,
+    <PromoCard />,
+    <PromoCard />,
+    <PromoCard />,
+    <PromoCard />,
+    <PromoCard />,
+  ];
+
   return (
     <main>
       {/* Header */}
-      <div>
+      <header>
         <div className="flex justify-between items-center mt-14 mx-6">
           <div className="flex flex-col justify-center gap-2">
             <p>Selamat Datang,</p>
@@ -21,9 +78,20 @@ const Homepage = () => {
         <div className="mx-6 mt-6">
           <Search />
         </div>
-      </div>
+      </header>
+
+      {/* Promo Section */}
+      <section>
+        <div className="mx-6 mt-6">
+          <h6 className="font-semibold font-jakarta">Promo Spesial</h6>
+        </div>
+
+        <div className="mx-6 mt-6">
+          <SimpleCarousel items={promoItems} itemsToShow={3} />
+        </div>
+      </section>
     </main>
   );
 };
 
-export default Homepage;
+export default Home;
