9c587d3691ed56af58bd03e0b5ad5d3680cf5e30
Gustitreshana <gustith2105@gmail.com>
2024-10-21 15:10:12 +0700

    refactor: change using map for choose several icons

--- amimum/src/components/Icon/index.tsx
+++ amimum/src/components/Icon/index.tsx
@@ -1,17 +1,24 @@
 import Image from "next/image";
 import bag2 from "../../../public/assets/icons/bag-2.svg";
 import notification from "../../../public/assets/icons/notification.svg";
+import arrowDown from "../../../public/assets/icons/arrow-down.svg";
 
 interface IconProps {
-    icon: string;
+  icon: keyof typeof iconMap;
 }
 
+const iconMap = {
+  bag2: bag2,
+  notification: notification,
+  arrowDown: arrowDown,
+};
+
 const Icon = ({ icon }: IconProps) => {
-    return (
-        <div>
-            <Image src={icon === "bag2" ? bag2 : notification} alt="icon" width={30} height={30} />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Image src={iconMap[icon]} alt="icon" width={30} height={30} />
+    </div>
+  );
+};
 
 export default Icon;
