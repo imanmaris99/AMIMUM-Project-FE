2d56ea567d3a3a31abef52fe1415f83474b6ce02
Gustitreshana <gustith2105@gmail.com>
2024-10-21 15:08:41 +0700

    feat: add category and production section

--- amimum/src/app/page.tsx
+++ amimum/src/app/page.tsx
@@ -1,52 +1,17 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
 import Icon from "@/components/Icon";
 import Search from "@/components/Search";
+import Carousel from "@/components/Carousel";
 import PromoCard from "@/components/PromoCard";
-
-const SimpleCarousel = ({
-  items,
-  itemsToShow,
-  interval = 4000,
-}: {
-  items: any[];
-  itemsToShow: number;
-  interval?: number;
-}) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const slideInterval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + itemsToShow) % items.length);
-    }, interval);
-
-    return () => clearInterval(slideInterval);
-  }, [itemsToShow, items.length, interval]);
-  
-  return (
-    <div className="carousel">
-      <div className="carousel-items" style={{ transform: `translateX(-${currentIndex * (100 / itemsToShow)}%)` }}>
-        {items.map((item, index) => (
-          <div key={index} className="carousel-item">
-            {item}
-          </div>
-        ))}
-      </div>
-      <div className="carousel-indicators flex justify-center mt-2">
-        {Array.from({ length: Math.ceil(items.length / itemsToShow) }).map((_, index) => (
-          <button
-            key={index}
-            className={`w-2 h-2 rounded-full mx-1 mt-2 ${index === Math.floor(currentIndex / itemsToShow) ? 'bg-customGreen1' : 'bg-gray-300'}`}
-            onClick={() => setCurrentIndex(index * itemsToShow)}
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
+import Label from "@/components/Label";
+import ProductionCard from "@/components/ProductionCard";
+import { useState } from "react";
 
 const Home = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [visibleItems, setVisibleItems] = useState(8); // State untuk melacak jumlah item yang ditampilkan
+
   const promoItems = [
     <PromoCard />,
     <PromoCard />,
@@ -57,7 +22,43 @@ const Home = () => {
     <PromoCard />,
     <PromoCard />,
     <PromoCard />,
+    <PromoCard />,
+    <PromoCard />,
+    <PromoCard />,
   ];
+
+  const categories = [
+    "Kategori 1",
+    "Kategori 2",
+    "Kategori 3",
+    "Kategori 4",
+    "Kategori 5",
+  ];
+
+  const productionItems = [
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+    <ProductionCard />,
+  ];
+
+  const loadMoreItems = () => {
+    setVisibleItems((prevVisibleItems) => prevVisibleItems + 9); // Tambah 9 item lagi
+  };
 
   return (
     <main>
@@ -83,11 +84,55 @@ const Home = () => {
       {/* Promo Section */}
       <section>
         <div className="mx-6 mt-6">
-          <h6 className="font-semibold font-jakarta">Promo Spesial</h6>
+          <h6 className="font-semibold font-jakarta">Promo spesial</h6>
         </div>
 
         <div className="mx-6 mt-6">
-          <SimpleCarousel items={promoItems} itemsToShow={3} />
+          <Carousel items={promoItems} itemsToShow={3} />
+        </div>
+      </section>
+
+      {/* Kategori Section */}
+      <section>
+        <div className="mx-6 mt-6">
+          <h6 className="font-semibold font-jakarta">Kategori</h6>
+        </div>
+
+        <div className="mx-6 mt-6 flex gap-2 overflow-x-auto hide-scrollbar whitespace-nowrap">
+          {categories.map((category, index) => (
+            <Label
+              key={index}
+              title={category}
+              isSelected={selectedCategory === category}
+              onClick={() => setSelectedCategory(category)}
+            />
+          ))}
+        </div>
+      </section>
+
+      {/* Produksi Section */}
+      <section>
+        <div className="mx-6 mt-6">
+          <h6 className="font-semibold font-jakarta">Produksi oleh</h6>
+        </div>
+
+        <div className="mx-6 mt-6 grid grid-cols-3 gap-4">
+          {productionItems.slice(0, visibleItems).map((_, index) => (
+            <ProductionCard key={index} />
+          ))}
+          {visibleItems < productionItems.length && (
+            <div className="flex flex-col justify-center items-center bg-customGreen5 rounded-lg gap-4 p-2" onClick={loadMoreItems}>
+              <div className="flex justify-center items-center bg-white rounded-full w-12 h-12">
+                <Icon icon="arrowDown" />
+              </div>
+
+              <div>
+                <button className="text-sm">
+                  Muat Lainnya
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </section>
     </main>
