9eba547a4d598bd7651ffcd5e4b085b01276bf1e
Gustitreshana <gustith2105@gmail.com>
2024-10-30 16:32:23 +0700

    feat: add a Navbar to all pages (login and registration pages may need modification to avoid being affected by the navbar)

--- amimum/src/app/layout.tsx
+++ amimum/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,6 +26,7 @@ export default function RootLayout({
       </head>
       <body className="antialiased font-jakarta font-inter">
         {children}
+        {/* <Navbar /> */}
       </body>
     </html>
   );
