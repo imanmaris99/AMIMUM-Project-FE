e462bcab871828834e382cea472cbd5adc0660ec
Gustitreshana <gustith2105@gmail.com>
2024-10-31 17:43:36 +0700

    refactor: remove folder Icons in public and change the icon with react-icon

--- amimum/src/components/Search/index.tsx
+++ amimum/src/components/Search/index.tsx
@@ -2,7 +2,8 @@
 
 import Button from "../Button";
 import Image from "next/image";
-import searchIcon from "../../../public/assets/icons/search-normal.svg";
+import { CiSearch } from "react-icons/ci";
+
 
 const handleSearch = () => {
     alert("duh, belum bisa search euy!");
@@ -11,7 +12,7 @@ const handleSearch = () => {
 const Search = () => {
     return (
         <div className="flex items-center justify-center border border-gray-200 rounded-lg px-2 py-2">
-            <Image className="ml-2" src={searchIcon} alt="search" width={20} height={20} />
+            <CiSearch className="w-8 h-8 text-gray-500 ml-2" />
             <input className="w-full px-2 focus:outline-none text-lg placeholder:text-sm" type="text" placeholder="Cari produk" />
             <Button onClick={handleSearch} />
         </div>
