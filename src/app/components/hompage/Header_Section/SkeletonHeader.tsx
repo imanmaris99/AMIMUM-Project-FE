a5573bd7a2e57397eb620c1b8389f2efcc0bd67e
Gustitreshana <gustith2105@gmail.com>
2024-11-13 00:26:54 +0700

    create skeleton header for welcome message

---
+++ src/app/components/hompage/Header_Section/SkeletonHeader.tsx
@@ -0,0 +1,8 @@
+export const SkeletonHeader = () => {
+  return (
+    <>
+      <div className="w-24 h-4 bg-gray-300 rounded animate-pulse"></div>
+      <div className="w-48 h-6 bg-gray-300 rounded animate-pulse"></div>
+    </>
+  );
+};
--- src/app/components/hompage/Header_Section/index.tsx
+++ src/app/components/hompage/Header_Section/index.tsx
@@ -1,17 +1,26 @@
 import { IoBagOutline, IoNotificationsOutline } from "react-icons/io5";
 import { Search } from "@/app/components";
 import { useUserProfile } from "@/hooks/useUserProfile";
+import { SkeletonHeader } from "./SkeletonHeader";
 
 const Header = () => {
-    const { userProfile } = useUserProfile();
+  const { userProfile, isLoading } = useUserProfile();
 
-    return (
+  return (
     <>
       <header>
         <div className="flex justify-between items-center mt-14 mx-6">
           <div className="flex flex-col justify-center gap-2">
-            <p>Selamat Datang,</p>
-            <h4 className="font-bold text-xl font-jakarta">{userProfile?.firstname} {userProfile?.lastname}</h4>
+            {isLoading ? (
+              <SkeletonHeader />
+            ) : (
+              <>
+                <p>Selamat Datang,</p>
+                <h4 className="font-bold text-xl font-jakarta">
+                  {userProfile?.firstname} {userProfile?.lastname}
+                </h4>
+              </>
+            )}
           </div>
 
           <div className="flex justify-center items-center gap-3">
