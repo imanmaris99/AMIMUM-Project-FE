f9816326a352ec118dd88a2f509a15c65ea3d6fc
Gustitreshana <gustith2105@gmail.com>
2024-11-13 14:36:58 +0700

    get data from API and skeleton when loading

--- src/app/components/hompage/ProductionCard/index.tsx
+++ src/app/components/hompage/ProductionCard/index.tsx
@@ -1,19 +1,23 @@
 import Image from "next/image";
-const ProductionCard = () => {
+import { ProductionProps } from "@/types/apiTypes";
+
+const ProductionCard = ({ production }: { production: ProductionProps }) => {
   return (
     <div className="bg-white rounded-lg h-36 flex flex-col items-center justify-center w-24 shadow-md">
       <div className="bg-gray-100 rounded-lg px-3 py-2 flex flex-col justify-center items-center">
-        <Image src="/jamu_jago_1.png" alt="promo" width={50} height={50} />
+        <Image src={production.photo_url} alt={production.name} width={50} height={50} />
       </div>
 
       <div className="flex flex-col justify-center items-center mt-2 gap-1">
         <div>
-          <p className="font-jakarta text-xs font-semibold">Jamu Jago</p>
+          <p className="font-jakarta text-xs font-semibold">
+            {production.name}
+          </p>
         </div>
 
         <div>
           <p className="font-jakarta text-[8px] text-gray-500">
-            Herbal olahan pabrik
+            {production.category}
           </p>
         </div>
       </div>
--- src/app/components/hompage/Production_Section/index.tsx
+++ src/app/components/hompage/Production_Section/index.tsx
@@ -3,14 +3,14 @@
 import { useState } from "react";
 import { ProductionCard } from "@/app/components";
 import { GoChevronDown } from "react-icons/go";
+import { useProductions } from "@/hooks/useProductions";
+import { ProductionProps } from "@/types/apiTypes";
+import ProductionCardSkeleton from "@/app/components/hompage/ProductionCard/ProductionCardSkeleton";
 
 const Production = () => {
+  const { productions, isLoading } = useProductions();
   const [visibleItems, setVisibleItems] = useState(8);
-  const productionItems = Array.from({ length: 27 }, (_, index) => (
-    <ProductionCard key={index} />
-  ));
 
-  const remainingItems = productionItems.length - visibleItems;
   const loadMoreItems = () => {
     setVisibleItems((prevVisibleItems) => prevVisibleItems + 9);
   };
@@ -22,8 +22,14 @@ const Production = () => {
       </div>
 
       <div className="mx-6 mt-6 mb-6 grid grid-cols-3 gap-4">
-        {productionItems.slice(0, visibleItems)}
-        {visibleItems < productionItems.length && (
+        {isLoading ? (
+          [...Array(9)].map((_, index) => <ProductionCardSkeleton key={index} />)
+        ) : (
+          productions?.slice(0, visibleItems).map((production: ProductionProps, index: number) => (
+            <ProductionCard key={index} production={production} />
+          ))
+        )}
+        {productions && productions.length > 9 && visibleItems < productions.length && (
           <div
             className="flex flex-col justify-center items-center bg-customGreen5 rounded-lg gap-4 p-2"
             onClick={loadMoreItems}
@@ -34,7 +40,7 @@ const Production = () => {
 
             <div>
               <button className="text-sm">
-                Muat Lainnya ({remainingItems})
+                Muat Lainnya ({productions.length - visibleItems})
               </button>
             </div>
           </div>
