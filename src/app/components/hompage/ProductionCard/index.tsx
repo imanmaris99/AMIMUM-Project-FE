58784b0c86e4ba669b0d08835ccc335456160963
Gustitreshana <gustith2105@gmail.com>
2024-11-15 01:40:44 +0700

    set default for photo if data is null

--- src/app/components/hompage/ProductionCard/index.tsx
+++ src/app/components/hompage/ProductionCard/index.tsx
@@ -5,7 +5,7 @@ const ProductionCard = ({ production }: { production: ProductionProps }) => {
   return (
     <div className="bg-white rounded-lg h-36 flex flex-col items-center justify-center w-24 shadow-md">
       <div className="bg-gray-100 rounded-lg px-3 py-2 flex flex-col justify-center items-center">
-        <Image src={production.photo_url} alt={production.name} width={50} height={50} />
+        <Image src={production.photo_url || ""} alt={production.name} width={50} height={50} />
       </div>
 
       <div className="flex flex-col justify-center items-center mt-2 gap-1">
