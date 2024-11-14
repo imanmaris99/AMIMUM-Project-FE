5b8fe17fa75f9744c013b04ee5be4ddc74ea0239
Gustitreshana <gustith2105@gmail.com>
2024-11-15 02:28:17 +0700

    add default image

--- src/app/components/hompage/ProductionCard/index.tsx
+++ src/app/components/hompage/ProductionCard/index.tsx
@@ -1,22 +1,23 @@
 import Image from "next/image";
 import { ProductionProps } from "@/types/apiTypes";
+import styles from "./ProductionCard.module.css";
 
 const ProductionCard = ({ production }: { production: ProductionProps }) => {
   return (
     <div className="bg-white rounded-lg h-36 flex flex-col items-center justify-center w-24 shadow-md">
-      <div className="bg-gray-100 rounded-lg px-3 py-2 flex flex-col justify-center items-center">
-        <Image src={production.photo_url || ""} alt={production.name} width={50} height={50} />
+      <div className="bg-gray-100 rounded-lg px-3 py-2 flex flex-col justify-center items-center w-20 h-20">
+        <Image src={production.photo_url || "/default-image.jpg"} alt={production.name} width={50} height={50} />
       </div>
 
       <div className="flex flex-col justify-center items-center mt-2 gap-1">
         <div>
-          <p className="font-jakarta text-xs font-semibold">
+          <p className={`font-jakarta text-xs font-semibold text-center ${styles.textEllipsis}`}>
             {production.name}
           </p>
         </div>
 
         <div>
-          <p className="font-jakarta text-[8px] text-gray-500">
+          <p className={`font-jakarta text-[8px] text-gray-500 text-center ${styles.textEllipsis}`}>
             {production.category}
           </p>
         </div>
--- src/app/components/hompage/PromoCard/index.tsx
+++ src/app/components/hompage/PromoCard/index.tsx
@@ -7,7 +7,7 @@ const PromoCard = ({ promo }: { promo: PromoProps }) => {
     <>
       <div className="bg-customGreen5 rounded-lg h-36 flex flex-col items-center justify-center w-24">
         <div className="bg-white rounded-lg px-3 py-2 flex flex-col justify-center items-center w-20 h-20">
-          <Image src={promo.photo_url} alt="promo" width={50} height={50} />
+          <Image src={promo.photo_url || "/default-image.jpg"} alt="promo" width={50} height={50} />
         </div>
 
         <div className="flex flex-col justify-center items-center mt-2 gap-1">
