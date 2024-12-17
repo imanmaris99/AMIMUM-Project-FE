691c25429b5b5478c6e3840f2eb15ac95151ca44
Muhammad Haikal Bintang <mhaikalbintang.work@gmail.com>
2024-12-17 17:30:41 +0700

    Avoided swr warning by adding "use client"

--- src/hooks/useArticles.ts
+++ src/hooks/useArticles.ts
@@ -1,25 +1,15 @@
-import useSWR from "swr";
+"use client";
 
+import useSWR from "swr";
 import { fetchArticles } from "@/lib/api/articles";
 
-
-
 export const useArticles = () => {
-
     const { data, error } = useSWR("/articles/all", fetchArticles, {
-
         errorRetryCount: 0,
-
     });
-
     return {
-
         articles: data,
-
         isLoading: !data && !error,
-
         isError: error?.response?.status
-
     };
-
 };
