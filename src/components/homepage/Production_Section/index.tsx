b402882e752919a94e06fd6c1a2480c0c7e8e7d8
Gustitreshana <gustith2105@gmail.com>
2024-12-27 15:38:17 +0700

    [REFINE] - Improve code readability in Production section by restructuring component layout and enhancing error message display, ensuring better maintainability and user experience.

--- src/components/homepage/Production_Section/index.tsx
+++ src/components/homepage/Production_Section/index.tsx
@@ -7,11 +7,22 @@ import getFilteredProductions from "@/utils/getFilteredProductions";
 import LoadMoreButton from "./LoadMoreButton";
 import ProductionList from "./ProductionList";
 
-const Production = ({ selectedCategory }: { selectedCategory: string | null }) => {
-  const { data: productions, loading: isLoading, errorMessage } = useBrandLoader();
+const Production = ({
+  selectedCategory,
+}: {
+  selectedCategory: string | null;
+}) => {
+  const {
+    data: productions,
+    loading: isLoading,
+    errorMessage,
+  } = useBrandLoader();
   const [visibleItems, setVisibleItems] = useState(8);
 
-  const filteredProductions = getFilteredProductions(productions || [], selectedCategory);
+  const filteredProductions = getFilteredProductions(
+    productions || [],
+    selectedCategory
+  );
 
   const loadMoreItems = () => {
     setVisibleItems((prevVisibleItems) => prevVisibleItems + 9);
@@ -23,7 +34,9 @@ const Production = ({ selectedCategory }: { selectedCategory: string | null }) =
         <div className="mx-6 mt-6">
           <h6 className="font-semibold font-jakarta">Produksi oleh</h6>
         </div>
-        <div className="mx-6 mt-6 text-red-500 font-semibold">{errorMessage}</div>
+        <div className="mx-6 mt-6 text-red-500 font-semibold">
+          {errorMessage}
+        </div>
       </>
     );
   }
@@ -34,11 +47,33 @@ const Production = ({ selectedCategory }: { selectedCategory: string | null }) =
         <h6 className="font-semibold font-jakarta">Produksi oleh</h6>
       </div>
 
-      <div className="mx-6 mt-6 mb-6 text-gray-500">{!isLoading && filteredProductions?.length === 0 && <div className="text-center text-sm">Tidak ada produksi yang sesuai dengan kategori ini.</div>}</div>
-
+      <div className="mx-6 mt-6 mb-6 text-gray-500">
+        {!isLoading && filteredProductions?.length === 0 && (
+          <div className="text-center text-sm">
+            Tidak ada merek yang sesuai dengan kategori ini.
+          </div>
+        )}
+      </div>
       <div className="mx-6 mt-6 mb-6 grid grid-cols-3 gap-4 justify-items-center">
-        {isLoading ? [...Array(9)].map((_, index) => <ProductionCardSkeleton key={index} />) : <ProductionList productions={filteredProductions} visibleItems={visibleItems} />}
-        {productions && filteredProductions && filteredProductions.length > 8 && visibleItems < filteredProductions.length && <LoadMoreButton onClick={loadMoreItems} remainingItems={filteredProductions.length - visibleItems} />}
+        {isLoading ? (
+          [...Array(9)].map((_, index) => (
+            <ProductionCardSkeleton key={index} />
+          ))
+        ) : (
+          <ProductionList
+            productions={filteredProductions}
+            visibleItems={visibleItems}
+          />
+        )}
+        {productions &&
+          filteredProductions &&
+          filteredProductions.length > 8 &&
+          visibleItems < filteredProductions.length && (
+            <LoadMoreButton
+              onClick={loadMoreItems}
+              remainingItems={filteredProductions.length - visibleItems}
+            />
+          )}
       </div>
     </>
   );
